940facfd05888af80c23464fe7608b29aac60030
TMWEE <50554058+TMWEE@users.noreply.github.com>
2021-01-10 17:33:07 +0530

    Driver Completed UIs

--- views/driver/accept-orders.php
+++ views/driver/accept-orders.php
@@ -1,7 +1,3 @@
-
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,7 +19,7 @@
         background-color: #dddddd;
         }
         .button {
-            background-color: blue;
+            background-color: black;
             border: none;
             color: white;
             padding: 15px 32px;
@@ -71,27 +67,27 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/dash.css">
+    <link rel="stylesheet" href="../assets/css/driver/dash.css">
     <title>Order Inbox/Accept Orders</title>
 </head>
 <body>
     <div class="driver-dashboard">
-        <?php include('nav/accept_orders_nav.php'); ?>
+        <?php include('dash-include/accept_orders_nav.php'); ?>
         <div class="content">
             <div class="container">
-                <a href="accept_orders.php" class="card" id="card1" style="display: block;">
+                <a href="accept-orders" class="card" id="card1" style="display: block;">
                     <i class="fas fa-inbox"></i>
                     <div class="container">
                         <h4><b>Accept Orders</br>3</b ></h4>
                     </div>
                 </a>
-                <a href="delivery_history.php" class="card" id="card2" style="display: block;">
+                <a href="delivery-history" class="card" id="card2" style="display: block;">
                     <i class="fas fa-list-alt"></i>
                     <div class=" container">
                         <h4><b>Delivery History</br></b></h4>
                     </div>
                  </a>
-                <a href="earnings.php" class="card" id="card3" style="display: block;">
+                <a href="earnings" class="card" id="card3" style="display: block;">
                     <i class="fas fa-money-check-alt"></i>
                     <div class="container">
                         <h4><b>Earnings</br>350.00</b></h4>
@@ -166,13 +162,10 @@
                 <td></td>
                 <td></td>
                 <td></td>
-                <td><a href="delivery_history.php"><button type="button" class="button">Order History</button></a></td>
+                <td><a href="delivery-history"><button type="button" class="button">Order History</button></a></td>
             </tr>
 
             </table>
     </div>    
-
-
-    <?php include('foot.php'); ?>
 </body>
 </html>
---
+++ views/driver/dash-include/contact_admin_nav.php
@@ -0,0 +1,30 @@
+<div class="navbar">
+    <div class="logo">
+        <img src="../assets/images/driver/logo.png" alt="logo" width="125px">
+    </div>
+    <div class="nav-right">
+        <h3>Hi Driver</h3>
+            <div class="dropdown">
+                <button class="dropbtn"><img  src="../assets/images/driver/driver_icon.png" alt="user" width="50px"></button>
+                <div class="dropdown-content">
+                    <a href="my-profile">My Profile</a>
+                    <a href="../../logout.php">Log Out</a>
+                </div>
+            </div>
+    </div>
+</div>
+<div class="sidebar">
+    <ul>
+        <li> <a href="dash">DASHBOARD</a></li>
+        <li><a href="edit-profile">Edit Profile</a></li>
+        <li><a href="update-location">Update Location</a></li>
+        <li>Orders
+            <ul style="display: inline;">
+                <li><a href="accept-orders">Order Inbox</a></li>
+                <li><a href="delivery-history">Delivery History</a></li>
+                <li><a href="earnings">Earnings</a></li>
+            </ul>
+        </li>
+        <li><m><a href="contact-admin">Contact Administrator</a></m></li>
+    </ul>
+</div>
